3b3b357d0f43e9f3f62e502de5b6bdd6a30446b9
PietroCampodonico <pietro.campodonico@outlook.it>
2021-04-29 18:29:01 +0200

    esercizio completato

---
+++ script.js
@@ -0,0 +1,28 @@
+var app = new Vue(
+{
+    el: "#root",
+    data: {
+            wrapperStyles: "wrapperStyles",
+            greetings: "hello from Vue!",
+            vulcanHand:"🖖",
+            h1Styles: "h1Styles",
+            handStyles: "handStyles",
+            btn: "Click to be Greeted",
+            displayContent: "none"
+        },
+
+    methods: {
+        saluta: function () {
+            if (this.displayContent == "none") {
+              this.displayContent = "inline-block",
+              this.btn = "click to Reset"
+
+            }  else {
+                this.displayContent !== "none",
+                    this.displayContent = "none"
+                    this.btn = "Click to be Greeted"
+            }
+            
+        }
+    }
+})
